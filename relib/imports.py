875a51f4a8ec77e9b91d6a1080a6e702d009cc9b
Hampus Hallman <me@hampushallman.com>
2017-08-07 17:54:52 +0200

    added ensure_dir and updated export functions to use it

--- relib/imports.py
+++ relib/imports.py
@@ -1,11 +1,18 @@
+import os
 import json
 import csv
+
+def ensure_dir(directory):
+  if not os.path.exists(directory):
+    os.makedirs(directory)
 
 def import_json(filename):
   with open(filename) as file:
     return json.load(file)
 
 def export_json(filename, data):
+  directory = '/'.join(filename.split('/')[:-1])
+  ensure_dir(directory)
   with open(filename, 'w') as file:
     json.dump(data, file)
 
@@ -15,6 +22,8 @@ def import_csv_as_dict_array(filename, delimiter=','):
     return [{k: v for k, v in row.items()} for row in reader]
 
 def export_csv_from_dict_array(filename, data):
+  directory = '/'.join(filename.split('/')[:-1])
+  ensure_dir(directory)
   keys = data[0].keys()
   with open(filename, 'w') as file:
     dict_writer = csv.DictWriter(file, keys)
