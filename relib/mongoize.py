89e7f69532f71ee55a85e76c6e4381b2ea8d7bdc
Hampus Hallman <me@hampushallman.com>
2017-08-20 21:03:07 +0200

    memoize: added option to use any of the provided storages

--- relib/mongoize.py
+++ relib/mongoize.py
@@ -7,7 +7,9 @@ import inspect
 from termcolor import colored
 
 storages = {'pickle': pickle_storage, 'bcolz': bcolz_storage, 'mongo': mongo_storage}
+
 storages['pickle'].initialize()
+storages['bcolz'].initialize()
 
 # Helper functions
 def get_function_body(func):
@@ -21,30 +23,30 @@ def get_hash(string):
   return ' [' + reutils.sha224(string) + ']'
 
 # Core mongoize
-def run_memoizer(func, name, force=False):
-  storage = storages['pickle']
+def run_memoizer(func, name, force=False, storage_format='pickle'):
+  storage = storages[storage_format]
+  storage_log = colored(storage_format, 'white', attrs=['dark'])
   if force or storage.get_is_expired(name):
     if force:
-      print(colored(' WITH FORCE ', 'grey', 'on_blue'), colored(name, 'blue'))
+      print(colored(' WITH FORCE ', 'grey', 'on_blue'), colored(name, 'blue'), storage_log)
     else:
-      print(colored(' MEMORIZING ', 'grey', 'on_blue'), colored(name, 'blue'))
+      print(colored(' MEMORIZING ', 'grey', 'on_blue'), colored(name, 'blue'), storage_log)
     data = func()
     storage.store_data(name, data)
     return data
   else:
-    print(colored(' REMEMBERED ', 'grey', 'on_green'), colored(name, 'green'))
+    print(colored(' REMEMBERED ', 'grey', 'on_green'), colored(name, 'green'), storage_log)
     return storage.load_data(name)
 
 # Memoize decorator
 func_by_wrapper = {}
 children_by_func = {}
-
-def get_function_from_wrapper(func):
-  return func_by_wrapper[func] if func in func_by_wrapper else func
+storage_format_by_func = {}
 
 def get_execution_tree(func):
   function_body = get_function_body(func)
-  children = children_by_func[func] if func in children_by_func else []
+  children = children_by_func[func]
+  storage_format = storage_format_by_func[func]
   children_response = f.map(children, get_execution_tree)
   function_bodies = '\n'.join([function_body] + [res['function_bodies'] for res in children_response])
   hash = func.__name__ + get_hash(function_bodies)
@@ -54,17 +56,18 @@ def get_execution_tree(func):
     return func(*params)
 
   def run():
-    return run_memoizer(wrapper_func, name=hash)
+    return run_memoizer(wrapper_func, name=hash, storage_format=storage_format)
 
   return {
     'function_bodies': function_bodies,
     'run': run
   }
 
-def memoize(*funcs):
+def memoize(*funcs, storage_format='pickle'):
   def _(func):
-    unwrapped_funcs = f.map(funcs, get_function_from_wrapper)
+    unwrapped_funcs = [func_by_wrapper[func] if func in func_by_wrapper else func for func in funcs]
     children_by_func[func] = unwrapped_funcs
+    storage_format_by_func[func] = storage_format
     run = get_execution_tree(func)['run']
     func_by_wrapper[run] = func
     return run
