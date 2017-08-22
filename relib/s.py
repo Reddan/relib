063acbd70fba3584d256be0508c340d616d1014e
Hampus Hallman <me@hampushallman.com>
2017-08-22 20:27:01 +0200

    added get_one_hotter_categories, create_one_hotter2

--- relib/s.py
+++ relib/s.py
@@ -34,3 +34,32 @@ def create_one_hotter(items, by_key, threshold=0.05):
       return create_normal(cat, multiplier)
 
   return one_hot, categories
+
+def get_one_hotter_categories(items, by_key, threshold=0):
+  values = [item[by_key] for item in items]
+  dominant_values, distinct_values = get_list_of_dominant_values(values, threshold)
+  exclude_other = len(dominant_values) == len(distinct_values)
+  categories = dominant_values if exclude_other else dominant_values + ['_OTHER_']
+  return categories
+
+def create_one_hotter2(categories, by_key):
+  normal_by_cat = {}
+
+  def create_normal(cat, multiplier):
+    if cat not in normal_by_cat:
+      normal_by_cat[cat] = {}
+    normal = [multiplier if cat == cat2 else 0 for cat2 in categories]
+    normal_by_cat[cat][multiplier] = normal
+    return normal
+
+  def one_hot(item, multiplier=1):
+    cat = item[by_key]
+    cat = cat if cat in categories else '_OTHER_'
+    if cat not in categories:
+      raise ValueError('Item contains a non existent category')
+    if cat in normal_by_cat and multiplier in normal_by_cat[cat]:
+      return normal_by_cat[cat][multiplier]
+    else:
+      return create_normal(cat, multiplier)
+
+  return one_hot
