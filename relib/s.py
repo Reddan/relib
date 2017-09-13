d2264a5baefa1a41461aa0d3c3251e99d3bfaa64
Hampus Hallman <me@hampushallman.com>
2017-09-13 13:52:45 +0200

    added grid_search

--- relib/s.py
+++ relib/s.py
@@ -1,5 +1,6 @@
 # s for statistics
 
+from . import f
 from collections import Counter
 
 def get_list_of_dominant_values(items, threshold=0.05):
@@ -63,3 +64,27 @@ def create_one_hotter2(categories, by_key):
       return create_normal(cat, multiplier)
 
   return one_hot
+
+def grid_search(make_params, fn):
+  initial_params_set = [f.make_combinations_by_dict(param_set) for param_set in make_params]
+
+  initial_params = {}
+  for params_set in initial_params_set:
+    initial_params = {**initial_params, **params_set[0]}
+
+  def grid_search(default_params, params_sets, scores_params_list=[]):
+    if len(params_sets) == 0:
+      return scores_params_list
+
+    uncleared_set = params_sets[0]
+    params_list = [{**default_params, **additional_params} for additional_params in uncleared_set]
+    scores = [fn(params) for params in params_list]
+    new_scores_params_list = f.dict_zip({'score': scores, 'params': params_list})
+    scores_params_list = sorted(new_scores_params_list + scores_params_list, key=lambda x: x['score'])
+    min_score_index = scores.index(min(scores))
+    additional_params = uncleared_set[min_score_index]
+    new_default_params = {**default_params, **additional_params}
+    return grid_search(new_default_params, params_sets[1:], scores_params_list)
+
+  return grid_search(initial_params, initial_params_set)
+
