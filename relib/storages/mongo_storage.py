9b9ad785960c15464b999143147fa98a1629cac1
Hampus Hallman <me@hampushallman.com>
2017-09-20 11:47:01 +0200

    drop expired mongo collections

--- relib/storages/mongo_storage.py
+++ relib/storages/mongo_storage.py
@@ -5,17 +5,23 @@ mongo = pymongo.MongoClient('mongodb://localhost/', connect=False).relib
 meta_store = mongo.meta_store
 
 def initialize():
-  meta = list(meta_store.find())
+  meta = meta_store.find()
   meta = [m for m in meta if get_is_expired(m['collection_name'])]
   for m in meta:
     mongo[m['collection_name']].drop()
 
-def get_is_expired(collection_name):
+def _get_is_expired(collection_name):
   collection_meta = meta_store.find_one({'collection_name': collection_name})
   now = datetime.datetime.now()
-  if collection_meta and collection_meta['expire_at']:
-    return now >= collection_meta['expire_at']
-  return False
+  if collection_meta:
+    if collection_meta['expire_at']:
+      return now >= collection_meta['expire_at']
+    return False
+  return True
+
+def get_is_expired(collection_name):
+  is_expired = _get_is_expired(collection_name)
+  return is_expired
 
 def store_data(collection_name, data, expire_in=None):
   expire_at = expire_in and datetime.datetime.now() + datetime.timedelta(seconds=expire_in)
@@ -24,9 +30,9 @@ def store_data(collection_name, data, expire_in=None):
     {'collection_name': collection_name, 'expire_at': expire_at},
     upsert=True
   )
-  collection.drop()
-  collection.insert_many(data)
-  return collection
+  mongo[collection_name].drop()
+  mongo[collection_name].insert_many(data)
+  return mongo[collection_name]
 
 def load_data(collection_name):
   return mongo[collection_name]
