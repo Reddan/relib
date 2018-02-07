51c113633fab7c42375c02ca5fe9e8f0446e3107
Hampus Hallman <me@hampushallman.com>
2018-02-07 17:57:22 +0100

    added PickleableKerasModel

--- relib/utils.py
+++ relib/utils.py
@@ -5,15 +5,16 @@ def get_tmp_path():
   file_name = str(random())[2:]
   return '/tmp/' + file_name
 
-def pickle_keras_model(model):
-  tmp_path = get_tmp_path()
-  model.save(tmp_path)
-  with open(tmp_path, 'rb') as file:
-    return BytesIO(file.read())
+class PickleableKerasModel():
+  def __init__(self, model):
+    tmp_path = get_tmp_path()
+    model.save(tmp_path)
+    with open(tmp_path, 'rb') as file:
+      self.wrapped_model = BytesIO(file.read())
 
-def keras_pickle_to_model(model_bytes):
-  from keras.models import load_model
-  tmp_path = get_tmp_path()
-  with open(tmp_path, 'wb') as file:
-    file.write(model_bytes.read())
-    return load_model(tmp_path)
+  def unwrap():
+    from keras.models import load_model
+    tmp_path = get_tmp_path()
+    with open(tmp_path, 'wb') as file:
+      file.write(self.wrapped_model.read())
+      return load_model(tmp_path)
