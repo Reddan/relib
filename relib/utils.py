cbbe559b9965f340ca1db84ffb0f057439557c15
Hampus Hallman <me@hampushallman.com>
2018-02-12 11:30:56 +0100

    unwrap PickleableKerasModel more than once

--- relib/utils.py
+++ relib/utils.py
@@ -1,5 +1,6 @@
 from io import BytesIO
 from random import random
+from copy import copy
 
 def get_tmp_path():
   file_name = str(random())[2:]
@@ -7,14 +8,15 @@ def get_tmp_path():
 
 class PickleableKerasModel():
   def __init__(self, model):
-    tmp_path = get_tmp_path()
+    tmp_path = get_tmp_path() + '.h5'
     model.save(tmp_path)
     with open(tmp_path, 'rb') as file:
       self.wrapped_model = BytesIO(file.read())
 
   def unwrap(self):
     from keras.models import load_model
-    tmp_path = get_tmp_path()
+    tmp_path = get_tmp_path() + '.h5'
     with open(tmp_path, 'wb') as file:
-      file.write(self.wrapped_model.read())
+      data = copy(self.wrapped_model).read()
+      file.write(data)
       return load_model(tmp_path)
