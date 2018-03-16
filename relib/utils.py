c2664161b4d4fd769ea12be2da38e202ddb2c350
Hampus Hallman <me@hampushallman.com>
2018-03-16 16:20:42 +0100

    added get_tmp_directory and DirectoryBytesIO

--- relib/utils.py
+++ relib/utils.py
@@ -3,9 +3,37 @@ import os
 from io import BytesIO
 from copy import copy
 from subprocess import call
+from pathlib import Path
 
 def get_tmp_path():
   return tempfile.mkstemp()[1]
+
+def get_tmp_directory():
+  dir_path = get_tmp_path()
+  os.remove(dir_path)
+  os.makedirs(dir_path)
+  return dir_path + '/'
+
+class DirectoryBytesIO:
+  def __init__(self, directory):
+    p = Path(directory)
+    file_names = [x.name for x in p.iterdir()]
+
+    def to_bytes(path):
+      with open(path, 'rb') as file:
+        return BytesIO(file.read())
+
+    self.byte_map = {
+      file_name: to_bytes(directory + '/' + file_name)
+      for file_name in file_names
+    }
+
+  def unpack(self, directory):
+    for file_name in self.byte_map:
+      path = directory + '/' + file_name
+      with open(path, 'wb') as file:
+        data = copy(self.byte_map[file_name]).read()
+        file.write(data)
 
 class PickleableKerasModel():
   def __init__(self, model):
