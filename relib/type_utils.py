a3c2b7bcd62af7ea60e2fd75a085e3803f2933e5
Hampus Hallman <hampus@hallman.eu>
2026-04-15 13:11:44 +0200

    1.3.17 - coalesce

--- relib/type_utils.py
+++ relib/type_utils.py
@@ -1,14 +1,18 @@
 from typing import Any
-from .types import T
+from .types import T, T1, T2
 
 __all__ = [
   "as_any",
+  "coalesce",
   "ensure_tuple",
   "non_none",
 ]
 
 def as_any(obj: Any) -> Any:
   return obj
+
+def coalesce(obj: T1 | None, default: T2) -> T1 | T2:
+  return default if obj is None else obj
 
 def non_none(obj: T | None) -> T:
   assert obj is not None
